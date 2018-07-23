28ccfc687513ef280ce0c5cc8b0f79a745ada206
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-23 12:01:42 +0800

    添加营销活动类

---
+++ classes/Activities/MobileShake.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2018/7/23
+ * Time: 11:56 AM
+ */
+
+namespace Ecjia\App\Market\Activities;
+
+
+use Ecjia\App\Market\AbstractActivity;
+
+class MobileShake extends AbstractActivity
+{
+
+    /**
+     * 代号标识
+     * @var string
+     */
+    protected $code = 'mobile_shake';
+
+    /**
+     * 名称
+     * @var string
+     */
+    protected $name = '手机摇一摇';
+
+    /**
+     * 描述
+     * @var string
+     */
+    protected $description = '通过手机摇一摇参与活动';
+
+    /**
+     * 图标
+     * @var string
+     */
+    protected $icon = '/statics/images/xxxxx.png'; //图片未添加
+
+
+    public function run() {
+
+
+
+    }
+
+
+}
---
+++ classes/Activities/WechatDaZhuanPan.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2018/7/23
+ * Time: 12:00 PM
+ */
+
+namespace Ecjia\App\Market\Activities;
+
+
+class WechatDaZhuanPan
+{
+
+    /**
+     * 代号标识
+     * @var string
+     */
+    protected $code = 'wechat_dazhuangpan';
+
+    /**
+     * 名称
+     * @var string
+     */
+    protected $name = '微信大转盘';
+
+    /**
+     * 描述
+     * @var string
+     */
+    protected $description = '在微信上参与的大转盘抽奖活动';
+
+    /**
+     * 图标
+     * @var string
+     */
+    protected $icon = '/statics/images/xxxxx.png'; //图片未添加
+
+
+    public function run() {
+
+
+
+    }
+
+}
