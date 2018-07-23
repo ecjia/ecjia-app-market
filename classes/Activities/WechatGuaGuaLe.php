08e99b06397ab961c4b4cf852cc07889a75c0e59
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-23 12:04:29 +0800

    添加营销活动类

---
+++ classes/Activities/WechatGuaGuaLe.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2018/7/23
+ * Time: 12:00 PM
+ */
+
+namespace Ecjia\App\Market\Activities;
+
+
+class WechatGuaGuaLe
+{
+
+    /**
+     * 代号标识
+     * @var string
+     */
+    protected $code = 'wechat_guaguale';
+
+    /**
+     * 名称
+     * @var string
+     */
+    protected $name = '微信刮刮乐';
+
+    /**
+     * 描述
+     * @var string
+     */
+    protected $description = '在微信上参与的刮刮乐抽奖活动';
+
+    /**
+     * 图标
+     * @var string
+     */
+    protected $icon = '/statics/images/xxxxx.png'; //图片未添加
+
+
+    public function run() {
+
+
+
+    }
+
+}
