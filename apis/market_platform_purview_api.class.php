4074631de05b47771589fc19a5d6726d7efcc977
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-01 15:49:57 +0800

    调整公众平台的菜单

--- apis/market_platform_purview_api.class.php
+++ apis/market_platform_purview_api.class.php
@@ -57,7 +57,7 @@ class market_platform_purview_api extends Component_Event_Api {
         	array('action_name' => '营销活动管理', 'action_code' => 'market_activity_manage', 'relevance'   => ''),
         	array('action_name' => '营销活动更新', 'action_code' => 'market_activity_update', 'relevance'   => ''),
         	array('action_name' => '营销活动删除', 'action_code' => 'market_activity_delete', 'relevance'   => ''),
-        	array('action_name' => '活动记录管理', 'action_code' => 'activity_record_manage', 'relevance'   => ''),
+        	array('action_name' => '中奖记录管理', 'action_code' => 'activity_record_manage', 'relevance'   => ''),
         );
         return $purviews;
     }
