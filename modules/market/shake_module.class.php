b6ce6f37c7f44201cf74c6c54428606a80b6d2c1
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-09-17 11:32:16 +0800

    更换market模块API使用完整类名

--- modules/market/shake_module.class.php
+++ modules/market/shake_module.class.php
@@ -50,7 +50,7 @@ defined('IN_ECJIA') or exit('No permission resources.');
  * @author  zrl
  *
  */
-class shake_module extends api_front implements api_interface {
+class market_shake_module extends api_front implements api_interface {
     public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {
 		
 		$this->authSession();
